b7370a62ff5a4bb8069eb27b0dce4818f0fbd915
diyorsattarov <diyorsattarov@outlook.com>
2024-11-05 18:35:51 -0600

    Added tests to test_list main method

--- list/test/test_list.cpp
+++ list/test/test_list.cpp
@@ -6,6 +6,15 @@ int main() {
     my_lst.add(1);
     my_lst.add(2);
     my_lst.add(3);
-    std::cout << "helloworld: " << my_lst.get(1) << std::endl;
+    std::cout << "List contents: ";
+    for (size_t i = 0; i < my_lst.size(); ++i) { std::cout << my_lst.get(i) << " "; }
+    std::cout << std::endl;
+
+    my_lst.rem();
+    my_lst.rem();
+    std::cout << "After removal, list contents: ";
+    for (size_t i = 0; i < my_lst.size(); ++i) { std::cout << my_lst.get(i) << " "; }
+    std::cout << std::endl;
+    std::cout << my_lst.get(my_lst.size()+1) << std::endl;
     return 0;
 }
